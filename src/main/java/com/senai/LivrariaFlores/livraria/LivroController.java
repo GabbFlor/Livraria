b674f97787a3ba87fbaf15746b201cef369017a4
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2024-08-24 12:02:08 -0300

    Permissão do CORs
    Adicionando a permissão para que qualquer serviço de site ou app possa usar a API

--- src/main/java/com/senai/LivrariaFlores/livraria/LivroController.java
+++ src/main/java/com/senai/LivrariaFlores/livraria/LivroController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
+// Permitindo qualquer site de usar a API (se não usar, da CORs error no console do site)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LivroController {
 
 //    cria a referência para ser possível de chamar o service do livro
